2a04e4fbd6d265e1861263572cbc3c9f7ace868f
d0gkiller87 <32069685+d0gkiller87@users.noreply.github.com>
2022-02-05 01:13:05 +0800

    Clean code & Add better instructions

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ pip3 install -r requirements.txt
 
 ## Requirements
 - Python 3.x
-- Cookie-based authentication. Insert your cookie(s) in the source code, i.e.:
-```
-cookie = {
-	'remember_user_token' : 'xxxxx',
+- Cookie-based authentication. Place your cookie(s) in the source code, i.e.:
+```python
+cookies = {
 	'kktix_session_token_v2': 'xxxxx'
 }
 ```
@@ -19,10 +18,10 @@ python3 oktix.py [Event] [Ticket ID] [Ticket Quantity] [Sleep Seconds]
 ```
 
 ## Parameters
-- Event - 活動名稱 (可透過活動 url 取得) / e.g. sitcon2020
-- Ticket ID - 票卷資訊ID (可透過 F12 選取票卷 HTML 找到) / e.g. 234871
+- Event - 活動名稱 (可透過活動 url 取得) / e.g. `https://kktix.com/events/sitcon2020` -> sitcon2020
+- Ticket ID - 票卷資訊 ID (於能瀏覽票種的頁面，可嘗試 F12 選取票種 HTML，可能能在附近找到) / e.g. 234871
 - Ticket Quantity - 票卷數量 / e.g. 1
-- Sleep Seconds - 失敗時重試前等待秒數 / e.g. 1.5
+- Sleep Seconds - 重新搶票間隔秒數 / e.g. 1.5
 
 ## Screenshots
-![](https://github.com/vungsung/OkTix/blob/master/screenshots/1.png)
+![](/screenshots/1.png)
--- oktix.py
+++ oktix.py
@@ -1,66 +1,86 @@
 #!/usr/bin/env python3
-import requests, sys, time, urllib.parse, signal
 from bs4 import BeautifulSoup
+import urllib.parse
+import requests
+import signal
+import json
+import time
+import sys
 
-cookie = {
-	'remember_user_token' : '',
-	'kktix_session_token_v2': ''
+cookies = {
+    'kktix_session_token_v2': ''
 }
 
-def getCSRFToken(event):
-	r = requests.get('https://kktix.com/events/{}/registrations/new'.format(event), cookies = cookie)
-	soup = BeautifulSoup(r.text, 'html.parser')
-	csrfTag = soup.find('meta', attrs = {'name':"csrf-token"})
-	try:
-		return csrfTag['content']
-	except Exception as e:
-		print('getCSRFToken Failed: {}\n{}'.format(repr(e), r.text))
-		sys.exit(-1)
+def getCSRFToken( event ):
+    url = f"https://kktix.com/events/{ event }/registrations/new"
+    print( f"-> { url }" )
+    r = requests.get( url, cookies = cookies )
+    soup = BeautifulSoup( r.text, 'html.parser' )
+    csrfTag = soup.find( 'meta', attrs = { 'name' : 'csrf-token' } )
+    try:
+        return csrfTag['content']
+    except Exception as e:
+        raise RuntimeError( f"getCSRFToken Failed: { e }\n{ r.text }" )
 
-def getOrderToken(event, csrfToken, ticketId, ticketQuantity):
-	csrfToken = urllib.parse.quote(csrfToken)
-	orderInfo = """
-		{"tickets":[{"id":%s,"quantity":%s,"invitationCodes":[],"member_code":"","use_qualification_id":null}],"currency":"TWD","recaptcha":{},"agreeTerm":true}
-	""" % (ticketId, ticketQuantity)
-	r = requests.post('https://queue.kktix.com/queue/{}?authenticity_token={}'.format(event, csrfToken), cookies = cookie, data = orderInfo)
-	try:
-		return r.json()['token']
-	except:
-		print(r.text)
-		#print('getOrderToken Failed: {}\n{}'.format(repr(e), r.text))
-		return -1
+def getOrderToken( event, csrfToken, ticketId, ticketQuantity ):
+    csrfToken = urllib.parse.quote( csrfToken )
+    orderInfo = json.dumps({
+        "tickets": [{
+            "id": int( ticketId ),
+            "quantity": int( ticketQuantity ),
+            "invitationCodes": [],
+            "member_code": "",
+            "use_qualification_id": None
+        }],
+        "currency": "TWD",
+        "recaptcha": {},
+        "agreeTerm": True
+    })
+    url = f"https://queue.kktix.com/queue/{ event }?authenticity_token={ csrfToken }"
+    print( f"-> { url }" )
+    r = requests.post( url, cookies = cookies, data = orderInfo )
+    try:
+        return r.json().get( 'token' )
+    except Exception as e:
+        raise RuntimeError( f"getOrderToken Failed: { e }\n{ r.text }" )
 
-def getOrderId(orderToken):
-	r = requests.get('https://queue.kktix.com/queue/token/{}'.format(orderToken), cookies = cookie)
-	try:
-		return r.json()['to_param']
-	except Exception as e:
-		print('getOrderId Failed: {}\n{}'.format(repr(e), r.text))
-		sys.exit(-1)
+def getOrderId( orderToken ):
+    url = f"https://queue.kktix.com/queue/token/{ orderToken }"
+    print( f"-> { url }" )
+    r = requests.get( url, cookies = cookies )
+    try:
+        return r.json()['to_param']
+    except Exception as e:
+        raise RuntimeError( f"getOrderId Failed: { e }\n{ r.text }" )
 
-def signal_handler(signal, frame):
-	sys.exit()
-signal.signal(signal.SIGINT, signal_handler)
+def signal_handler( signal, frame ):
+    sys.exit()
+signal.signal( signal.SIGINT, signal_handler )
 
 def main():
-	if len(sys.argv) < 5:
-		print('Usage: python3 oktix.py [Event] [Ticket ID] [Ticket Quantity] [Sleep Seconds]')
-		sys.exit(-1)
+    if len( sys.argv ) < 5:
+        print( 'Usage: python3 oktix.py [Event] [Ticket ID] [Ticket Quantity] [Sleep Seconds]' )
+        return
 
-	event = sys.argv[1]
-	ticketId = sys.argv[2]
-	ticketQuantity = sys.argv[3]
-	sleepTime = float(sys.argv[4])
+    event = sys.argv[1]
+    ticketId = sys.argv[2]
+    ticketQuantity = sys.argv[3]
+    sleepTime = float( sys.argv[4] )
 
-	csrfToken = getCSRFToken(event)
-	while True:
-		orderToken = getOrderToken(event, csrfToken, ticketId, ticketQuantity)
-		time.sleep(sleepTime)
-		if not orderToken is -1:
-			break
-	orderId = getOrderId(orderToken)
+    csrfToken = getCSRFToken( event )
+    while True:
+        orderToken = getOrderToken( event, csrfToken, ticketId, ticketQuantity )
+        if orderToken:
+            break
+        time.sleep( sleepTime )
+    time.sleep( 1 )
+    orderId = getOrderId( orderToken )
 
-	print('https://kktix.com/events/{}/registrations/{}'.format(event, orderId))
+    print( f"[+] Success: https://kktix.com/events/{ event }/registrations/{ orderId }" )
 
 if __name__ == '__main__':
-	main()
+    try:
+        main()
+    except Exception as e:
+        raise e
+        print( f"[-] Error: { e }" )
